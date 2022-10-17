ac041b78e2c060332f641b4575c4f5308182faa7
Cam-Allan <113545870+Cam-Allan@users.noreply.github.com>
2022-10-17 17:43:31 +0100

    added S2 and S3 with arrays

---
+++ Strat3 with arrays.R
@@ -0,0 +1,49 @@
+st <- Sys.time() # Start Timer
+
+## initialisms
+n <- 50 # standard n
+nreps <- 10000 # standard nreps
+prisoner_numbers <- c(1:100) # create vector of prisoner numbers
+
+## simulaton initialisms
+s3boxes <- array(rep(0,nreps*2*n*n), dim = c(2*n,n,nreps)) # create an array of the cards pulled from the boxes. Each matrix is a simulation
+successes <- array(rep(0,2*n*nreps), dim = c(2*n,nreps)) # create array tracking tracking success of a prisoner escaping in a given simulation
+# note the indices are:
+# the rows, i, for each prisoner
+# the columns, j, for each choice of box
+# the matrices, k, for each simulation
+
+
+## simulation
+for (k in 1:nreps){ # for each simulation
+  box_contains <- sample(prisoner_numbers) # randomise cards in boxes
+  #strat3
+  for (i in prisoner_numbers){ # for each prisoner
+    s3boxes[i,,k] <- box_contains[sample(prisoner_numbers,n)]
+  } # prisoner has selected all n boxes now, need to check if they found their card
+  for (i in prisoner_numbers){ # for each prisoner
+    if (i %in% s3boxes[i,,k]){ # check if their card number is in their row
+      successes[i,k] <- 1 # if they found it, we say they were successful in that simulation
+    } else { # otherwise they were not successful
+      successes[i,k] <- 0 
+    }
+  }
+} # end of simulation
+
+## checking simulation was a success
+
+# create an array of the successes
+success_array <- array(successes,dim = c(2*n,nreps))
+
+# likelihood of each prisoner escaping individually
+length(which(successes == 1))/(2*n*nreps) 
+
+# create an array of the product of each simulation,
+# if any prisoner did not escape then not everyone escaped.
+allescape <- apply(success_array, MARGIN = 2, prod) 
+
+# check the probability everyone escapes is about 0.31
+length(which(allescape == 1))/nreps 
+
+et <- Sys.time() # end timer
+et - st # check time difference
